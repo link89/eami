d39137ae73095fb6ec36dd626e3e5dc421aad86e
Weihong,Xu <xuweihong.cn@gmail.com>
2015-12-15 23:47:39 +0800

    handle tcp data receiving

--- src/eami_client.erl
+++ src/eami_client.erl
@@ -1,9 +1,12 @@
 -module(eami_client).
+
 -behaviour(gen_server).
+
+-include("eami.hrl").
 
 -define(SERVER, ?MODULE).
 
--define(SOCKET_OPTS, [list, {active, once}, {packet, raw}, {reuseaddr, true}]).
+-define(SOCKET_OPTS, [binary, {active, once}, {packet, raw}, {reuseaddr, true}]).
 -define(RECV_TIMEOUT, 5000).
 
 
@@ -11,7 +14,7 @@
 %% API Function Exports
 %% ------------------------------------------------------------------
 
--export([start_link/7]).
+-export([start_link/8]).
 
 %% ------------------------------------------------------------------
 %% gen_server Function Exports
@@ -26,9 +29,12 @@
 
 -record(state, {host :: string() | undefined
                 ,port :: integer() | undefined
-                ,username :: string() | undefined
-                ,password :: string() | undefined
+                ,username :: binary() | string() | undefined
+                ,password :: binary() | string() | undefined
                 ,events :: on | off
+
+                ,on_event :: fun() | undefined
+                ,fragment :: binary()
 
                 ,reconnect_sleep :: integer() | undefined
                 ,connect_timeout :: integer() | undefined
@@ -37,6 +43,7 @@
                 ,self :: pid()
                 ,seq :: integer()
                }).
+-type state() :: #state{}.
 
 %% ------------------------------------------------------------------
 %% API Function Definitions
@@ -44,26 +51,30 @@
 
 -spec start_link(Host :: string()
                  ,Port :: integer()
-                 ,Username :: string()
-                 ,Password :: string()
+                 ,Username :: binary() | string()
+                 ,Password :: binary() | string()
                  ,Events :: on | off
+                 ,OnEvent :: fun() | undefined
                  ,ReconnectSleep :: integer() | undefined
                  ,ConnectTimeout :: integer() | undefined) ->
     {ok, Pid::pid()} | {error, Reason::term()}.
-start_link(Host, Port, Username, Events, Password, ReconnectSleep, ConnectTimeout) ->
-    gen_server:start_link(?MODULE, [Host, Port, Username, Password, Events,
+start_link(Host, Port, Username, Events, OnEvent, Password, ReconnectSleep, ConnectTimeout) ->
+    gen_server:start_link(?MODULE, [Host, Port, Username, Password, Events, OnEvent,
                                     ReconnectSleep, ConnectTimeout], []).
 
 %% ------------------------------------------------------------------
 %% gen_server Function Definitions
 %% ------------------------------------------------------------------
 
-init([Host, Port, Username, Password, Events, ReconnectSleep, ConnectTimeout]) ->
+init([Host, Port, Username, Password, Events, OnEvent, ReconnectSleep, ConnectTimeout]) ->
     State = #state{host = Host
                    ,port = Port
                    ,username = Username
                    ,password = Password
                    ,events = Events
+
+                   ,on_event = OnEvent
+                   ,fragment = <<>>
 
                    ,reconnect_sleep = ReconnectSleep
                    ,connect_timeout = ConnectTimeout
@@ -83,6 +94,42 @@ handle_call(_Request, _From, State) ->
 
 handle_cast(_Msg, State) ->
     {noreply, State}.
+
+%% Receive data from socket, see handle_response/2. Match `Socket' to
+%% enforce sanity.
+handle_info({tcp, Socket, Bs}, #state{socket = Socket} = State) ->
+    ok = inet:setopts(Socket, [{active, once}]),
+    {noreply, handle_response(Bs, State)};
+
+handle_info({tcp, Socket, _}, #state{socket = OurSocket} = State)
+  when OurSocket =/= Socket ->
+    %% Ignore tcp messages when the socket in message doesn't match
+    %% our state.
+    {noreply, State};
+
+handle_info({tcp_error, _Socket, _Reason}, State) ->
+    %% This will be followed by a close
+    {noreply, State};
+
+%% Socket got closed
+handle_info({tcp_closed, _Socket}, #state{reconnect_sleep = no_reconnect } = State) ->
+    %% If we aren't going to reconnect, then there is nothing else for
+    %% this process to do.
+    {stop, normal, State#state{socket = undefined}};
+
+handle_info({tcp_closed, _Socket}, State) ->
+    Self = self(),
+    spawn(fun() -> reconnect_loop(Self, State) end),
+
+    %% Throw away the socket and the queue, as we will never get a
+    %% response to the requests sent on the old socket. The absence of
+    %% a socket is used to signal we are "down"
+    {noreply, State#state{socket = undefined}};
+
+%% Redis is ready to accept requests, the given Socket is a socket
+%% already connected and authenticated.
+handle_info({connection_ready, Socket}, #state{socket = undefined} = State) ->
+    {noreply, State#state{socket = Socket}};
 
 handle_info(_Info, State) ->
     {noreply, State}.
@@ -116,10 +163,10 @@ connect(#state{host = Host
     end.
 
 authenticate(Socket, Username, Password, Events) ->
-    LoginAction = eami_util:encode_message([{"Action", "Login"}
-                                            ,{"Username", Username}
-                                            ,{"Secret", Password}
-                                            ,{"Events", Events}]),
+    LoginAction = eami_util:encode_message([{<<"Action">>, <<"Login">>}
+                                            ,{<<"Username">>, Username}
+                                            ,{<<"Secret">>, Password}
+                                            ,{<<"Events">>, Events}]),
     do_sync_command(Socket, LoginAction).
 
 do_sync_command(Socket, Command) ->
@@ -127,10 +174,10 @@ do_sync_command(Socket, Command) ->
     case gen_tcp:send(Socket, Command) of
         ok ->
             case gen_tcp:recv(Socket, 0, ?RECV_TIMEOUT) of
-                {ok, Raw} ->
-                    Event = eami_util:decode_message(Raw),
-                    case eami_util:get_value("Response", Event) of
-                        "Success" ->
+                {ok, Data} ->
+                    Event = eami_util:decode_message(Data),
+                    case eami_util:get_value(<<"Response">>, Event) of
+                        <<"Success">> ->
                             ok = inet:setopts(Socket, [{active, once}]);
                         Other ->
                             {error, {unexpected_data, Other}}
@@ -140,4 +187,29 @@ do_sync_command(Socket, Command) ->
             end;
         {error, Reason} ->
             {error, Reason}
+    end.
+
+-spec handle_response(binary(),state()) -> state().
+handle_response(Data, #state{on_event = OnEvent
+                             ,fragment = Fragment} = State) ->
+    case eami_util:parse(Data, Fragment) of
+        {Messages, NewFragment} ->
+            OnEvent(Messages),
+            State#state{fragment = NewFragment};
+        _ ->
+            State
+    end.
+
+%% @doc: Loop until a connection can be established
+reconnect_loop(Client, #state{reconnect_sleep = ReconnectSleep} = State) ->
+    case catch(connect(State)) of
+        {ok, #state{socket = Socket}} ->
+            gen_tcp:controlling_process(Socket, Client),
+            Client ! {connection_ready, Socket};
+        {error, _Reason} ->
+            timer:sleep(ReconnectSleep),
+            reconnect_loop(Client, State);
+        _ ->
+            timer:sleep(ReconnectSleep),
+            reconnect_loop(Client, State)
     end.
--- src/eami_util.erl
+++ src/eami_util.erl
@@ -1,21 +1,24 @@
 -module(eami_util).
 
--export([encode_message/1, decode_message/1, get_value/2]).
--export([to_list/1]).
+-include("eami.hrl").
 
--define(EOL, "\r\n").
+-export([encode_message/1, decode_message/1, get_value/2, parse/2]).
+-export([to_list/1, to_binary/1]).
+
+-define(EOL, <<"\r\n">>).
+-define(EOM, <<"\r\n\r\n">>).
 
 encode_message(Message) ->
     lists:foldr(
       fun({Key, Value}, Acc) ->
-              [to_list(Key), ":", to_list(Value), ?EOL | Acc]
+              [to_binary(Key), <<":">>, to_binary(Value), ?EOL | Acc]
       end, [], Message).
 
-decode_message(Raw) ->
-    Lines = string:tokens(Raw, ?EOL),
+decode_message(Data) ->
+    Lines = tokens(to_binary(Data), [?EOL, <<" ">>]),
     lists:filtermap(
       fun(Line) ->
-              case string:tokens(Line, ": ") of
+              case tokens(Line, [<<":">>, <<" ">>]) of
                   [Key, Value] -> {true, {Key, Value}};
                   _ -> false
               end
@@ -24,10 +27,33 @@ decode_message(Raw) ->
 get_value(Key, Message) ->
     proplists:get_value(Key, Message).
 
+-spec parse(binary(), binary()) -> {list(), binary()}.
+-spec parse(binary(), binary(), list()) -> {list(), binary()}.
+parse(Data, Fragment) ->
+    parse(Data, Fragment, []).
+
+parse(Data, Fragment, Messages) ->
+    case binary:split(<<Fragment/binary, Data/binary>>, [?EOM]) of
+        [RawMsg, Rest] ->
+            parse(Rest, <<>>, [encode_message(RawMsg) | Messages]);
+        [NewFragment] ->
+            {Messages, NewFragment}
+    end.
 
 -spec to_list(atom() | list() | binary() | integer() | float()) -> list().
 to_list(X) when is_list(X) -> X;
 to_list(X) when is_binary(X) -> binary_to_list(X);
 to_list(X) when is_atom(X) -> atom_to_list(X);
-to_list(X) when is_float(X) -> mochinum:digits(X);
-to_list(X) when is_integer(X) -> integer_to_list(X).
+to_list(X) when is_integer(X) -> integer_to_list(X);
+to_list(X) when is_float(X) -> mochinum:digits(X).
+
+-spec to_binary(atom() | string() | binary() | integer() | float() | pid() | iolist()) -> binary().
+to_binary(X) when is_binary(X) -> X;
+to_binary(X) when is_list(X) -> iolist_to_binary(X);
+to_binary(X) when is_atom(X) -> list_to_binary(atom_to_list(X));
+to_binary(X) when is_pid(X) -> to_binary(pid_to_list(X));
+to_binary(X) when is_integer(X) -> list_to_binary(integer_to_list(X));
+to_binary(X) when is_float(X) -> to_binary(mochinum:digits(X)).
+
+tokens(Bin, Pattern) ->
+    binary:split(Bin, Pattern, [global, trim_all]).
